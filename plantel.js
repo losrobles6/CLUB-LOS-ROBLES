770770840ee135ec956aabfe92c95d970448d3d0
losrobles6 <soytosti5@gmail.com>
2025-12-29 11:39:11 -0500

    Update plantel.js

--- plantel.js
+++ plantel.js
@@ -62,3 +62,11 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
